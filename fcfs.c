b0a55fa81bb99de9259f2ac85629ce5907ce1347
MITHRAVINDA U <mithravinda001@gmail.com>
2026-03-01 20:06:37 +0530

    Refactor FCFS scheduling algorithm implementation

--- fcfs.c
+++ fcfs.c
@@ -1,68 +1,65 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct {
-    int pid;
-    int arrival;
-    int burst;
-    int waiting;
-    int turnaround;
-} Process;
-
 int main() {
     int n;
     scanf("%d", &n);
-    
-    Process p[1000];
-    
-    // Read input
-    for(int i = 0; i < n; i++) {
-        scanf("%d %d %d", &p[i].pid, &p[i].arrival, &p[i].burst);
+
+    int pid[100], at[100], bt[100];
+    int wt[100], tat[100];
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d %d %d", &pid[i], &at[i], &bt[i]);
     }
-    
+
     // Sort by arrival time (bubble sort)
-    for(int i = 0; i < n - 1; i++) {
-        for(int j = 0; j < n - i - 1; j++) {
-            if(p[j].arrival > p[j+1].arrival) {
-                Process temp = p[j];
-                p[j] = p[j+1];
-                p[j+1] = temp;
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (at[j] > at[j + 1]) {
+                int tmp;
+                tmp = at[j];  at[j] = at[j+1];  at[j+1] = tmp;
+                tmp = bt[j];  bt[j] = bt[j+1];  bt[j+1] = tmp;
+                tmp = pid[j]; pid[j] = pid[j+1]; pid[j+1] = tmp;
             }
         }
     }
-    
-    // Calculate times
-    int current_time = 0;
-    float total_wt = 0, total_tat = 0;
-    
-    for(int i = 0; i < n; i++) {
-        // Handle CPU idle time
-        if(current_time < p[i].arrival) {
-            current_time = p[i].arrival;
+
+    // Compute waiting time and turnaround time
+    int currentTime = 0;
+    for (int i = 0; i < n; i++) {
+        if (currentTime < at[i]) {
+            currentTime = at[i];
         }
-        
-        int completion = current_time + p[i].burst;
-        p[i].turnaround = completion - p[i].arrival;
-        p[i].waiting = p[i].turnaround - p[i].burst;
-        
-        current_time = completion;
-        total_wt += p[i].waiting;
-        total_tat += p[i].turnaround;
+        wt[i] = currentTime - at[i];
+        tat[i] = wt[i] + bt[i];
+        currentTime += bt[i];
     }
-    
-    // Print EXACT format required
-    printf("Waiting Time: ");
-    for(int i = 0; i < n; i++) {
-        printf("P%d %d ", p[i].pid, p[i].waiting);
+
+    // Print Waiting Times
+    printf("Waiting Time:");
+    for (int i = 0; i < n; i++) {
+        printf(" P%d %d", pid[i], wt[i]);
     }
-    
-    printf("Turnaround Time: ");
-    for(int i = 0; i < n; i++) {
-        printf("P%d %d ", p[i].pid, p[i].turnaround);
+    printf("\n");
+
+    // Print Turnaround Times
+    printf("Turnaround Time:");
+    for (int i = 0; i < n; i++) {
+        printf(" P%d %d", pid[i], tat[i]);
     }
-    
-    printf("Average Waiting Time: %.2f Average Turnaround Time: %.2f\n", 
-           total_wt / n, total_tat / n);
-    
+    printf("\n");
+
+    // Compute averages
+    double avgWT = 0, avgTAT = 0;
+    for (int i = 0; i < n; i++) {
+        avgWT  += wt[i];
+        avgTAT += tat[i];
+    }
+    avgWT  /= n;
+    avgTAT /= n;
+
+    printf("Average Waiting Time: %.2f\n", avgWT);
+    printf("Average Turnaround Time: %.2f\n", avgTAT);
+
     return 0;
 }
