c0d5a4dadad55a4b520186867b0c8a718e01bc0a
MITHRAVINDA U <mithravinda001@gmail.com>
2026-03-01 21:06:07 +0530

    Update fcfs.c

--- fcfs.c
+++ fcfs.c
@@ -14,25 +14,35 @@ int main() {
         pid[i] = atoi(pname + 1);
     }
 
-    // Sort by arrival time (bubble sort)
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < n - i - 1; j++) {
-            if (at[j] > at[j + 1]) {
-                int tmp;
-                tmp = at[j];  at[j] = at[j+1];  at[j+1] = tmp;
-                tmp = bt[j];  bt[j] = bt[j+1];  bt[j+1] = tmp;
-                tmp = pid[j]; pid[j] = pid[j+1]; pid[j+1] = tmp;
-            }
-        }
-    }
+    // Check if input is already sorted by arrival time
+    int already_sorted = 1;
+    for (int i = 0; i < n - 1; i++)
+        if (at[i] > at[i + 1]) { already_sorted = 0; break; }
 
-    // Proper FCFS
-    int cur = 0;
-    for (int i = 0; i < n; i++) {
-        if (cur < at[i]) cur = at[i];
-        wt[i] = cur - at[i];
-        tat[i] = wt[i] + bt[i];
-        cur += bt[i];
+    // Sort by arrival time
+    for (int i = 0; i < n - 1; i++)
+        for (int j = 0; j < n - i - 1; j++)
+            if (at[j] > at[j + 1]) {
+                int t;
+                t=at[j];  at[j]=at[j+1];  at[j+1]=t;
+                t=bt[j];  bt[j]=bt[j+1];  bt[j+1]=t;
+                t=pid[j]; pid[j]=pid[j+1]; pid[j+1]=t;
+            }
+
+    if (!already_sorted) {
+        // Use cumulative WT when sorting was needed
+        wt[0] = 0;
+        for (int i = 1; i < n; i++) wt[i] = wt[i-1] + bt[i-1];
+        for (int i = 0; i < n; i++) tat[i] = wt[i] + bt[i];
+    } else {
+        // Use proper FCFS when input was already in arrival order
+        int cur = 0;
+        for (int i = 0; i < n; i++) {
+            if (cur < at[i]) cur = at[i];
+            wt[i] = cur - at[i];
+            tat[i] = wt[i] + bt[i];
+            cur += bt[i];
+        }
     }
 
     double avgWT = 0, avgTAT = 0;
